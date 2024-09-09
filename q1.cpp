a7949803eb2bf401389303a9ad7423c6c73ea4e3
YASHOVARDHANR <133276173+YASHOVARDHANR@users.noreply.github.com>
2024-09-09 15:10:47 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,15 @@
+
+#include<buts/stdc++.h>
+using namespace std;
+
+int main(){
+    string arr
+
+    reverse(arr.begin(),arr.end());
+
+    
+    cout<<arr<<endl;
+    
+
+    return 0;
+}
---
+++ q2.cpp
@@ -0,0 +1,18 @@
+#include<buts/stdc++.h>
+using namespace std;
+
+int main(){
+   int n;
+   cin>>n;
+   int arr[n];
+   for(int i=0;i<n;i++)cin>>arr[i];
+
+   int avg=0;
+   for(auto i : arr)avg+=i;
+   avg/=n;
+
+   cout<<ang<<endl;
+    
+
+    return 0;
+}
